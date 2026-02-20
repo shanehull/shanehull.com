a60d5e0401b30e6a871f5741d36173d5cc7644cd
shane.hull <shane.hull@remote.com>
2026-02-20 17:34:18 +1100

    chore: remove htmx debug

--- assets/js/htmx-debug.js
+++
@@ -1,8 +0,0 @@
-// Debug HTMX syntax errors
-if (window.htmx) {
-  window.htmx.on("htmx:syntax:error", function (evt) {
-    console.error("HTMX Syntax Error:", evt.detail);
-    console.error("Element:", evt.detail.elt);
-    console.error("Error message:", evt.detail.error);
-  });
-}
